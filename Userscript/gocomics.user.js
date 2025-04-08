930504fdfe19e3864b96d47f65866390fee19015
Ameir Al-Zoubi <ameir.alzoubi@gmail.com>
2025-04-08 09:14:05 -0400

    fix type

--- Userscript/gocomics.user.js
+++ Userscript/gocomics.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Gocomics Depaywall
 // @description Bypasses the Gocomics Paywall and Removes Ads
-// @icon        https://github.com/Idiot-01/Gocomics-Depaywall/raw/master/Extension/icons/icon48.png
+// @icon        https://github.com/z3bi/Gocomics-Depaywall/raw/master/Extension/icons/icon48.png
 // @match       *://*.gocomics.com/*
 // @grant       none
 // @version     1.2
@@ -11,28 +11,28 @@
 // @compatible  opera
 // @compatible  safari
 // @compatible  edge
-// @downloadURL https://github.com/Idiot-01/Gocomics-Depaywall/raw/master/Userscript/gocomics.user.js
-// @updateURL   https://github.com/Idiot-01/Gocomics-Depaywall/raw/master/Userscript/gocomics.user.js
+// @downloadURL https://github.com/z3bi/Gocomics-Depaywall/raw/master/Userscript/gocomics.user.js
+// @updateURL   https://github.com/z3bi/Gocomics-Depaywall/raw/master/Userscript/gocomics.user.js
 // @run-at      document-end
 // ==/UserScript==
 
 // Set overflow to auto to allow scroll
 (function() {
-    'use strict';
-    const style = document.createElement('style');
-    style.innerHTML = `
-      html, body {
-        overflow: auto !important;
-      }
-    `;
-    document.head.appendChild(style);
-  })();
-  
-  // gets ad and softpaywall <div> elements and hides them
-  const observer = new MutationObserver(() => {
-      document.querySelectorAll('div[class*="AdDisplay"], div[class*="HeaderAd"], div[class*="Upsell"]').forEach(el => el.style.display = "none");
-      document.querySelector('img[class*="Comic_comic__image_strip]').setAttribute('style', 'min-width: 900px;')
-  });
-  observer.observe(document.body, {childList: true, subtree: true});
-  
-  // chat ads hidden within closed shadow root, can't remove
+  'use strict';
+  const style = document.createElement('style');
+  style.innerHTML = `
+    html, body {
+      overflow: auto !important;
+    }
+  `;
+  document.head.appendChild(style);
+})();
+
+// gets ad and softpaywall <div> elements and hides them
+const observer = new MutationObserver(() => {
+    document.querySelectorAll('div[class*="AdDisplay"], div[class*="HeaderAd"], div[class*="Upsell"]').forEach(el => el.style.display = "none");
+    document.querySelector('img[class*="Comic_comic__image_strip"]').setAttribute('style', 'min-width: 900px !important;');
+});
+observer.observe(document.body, {childList: true, subtree: true});
+
+// chat ads hidden within closed shadow root, can't remove
